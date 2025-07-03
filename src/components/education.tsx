df336875e045e91ea26e8a10850932203beff20c
buoysophit <bouysophit11@gmail.com>
2025-07-03 19:16:59 +0700

    fixlogo

--- src/components/education.tsx
+++ src/components/education.tsx
@@ -10,7 +10,7 @@ const BLUR_FADE_DELAY = 0.04;
 export default function Education() {
   return (
     <section id="education">
-      <div className="flex min-h-0 flex-col gap-y-3">
+      <div className="flex min-h-0 flex-col gap-y-3 px-4 sm:px-6 lg:px-8">
         <BlurFade delay={BLUR_FADE_DELAY * 7}>
           <h2 className="text-xl font-bold">Education</h2>
         </BlurFade>
